4ed7671a49b1609f6a2e16871c12c4da29386263
Alex Klimaj <alex@arkelectron.com>
2024-07-03 17:06:16 -0600

    Add ARK 4IN1

--- Mcu/f051/Src/peripherals.c
+++ Mcu/f051/Src/peripherals.c
@@ -495,6 +495,31 @@ void initLed()
 }
 #endif
 
+#ifdef USE_DRV8328_NSLEEP // Disable gate driver when disarmed
+void initnSleep()
+{
+    LL_GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+    GPIO_InitStruct.Pin = NSLEEP_PIN;
+    GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+    LL_GPIO_Init(NSLEEP_PORT, &GPIO_InitStruct);
+    NSLEEP_PORT->BSRR = NSLEEP_PIN;
+}
+#endif
+
+#ifdef USE_DRV8328_NFAULT
+void initnFault()
+{
+    LL_GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+    GPIO_InitStruct.Pin = NFAULT_PIN;
+    GPIO_InitStruct.Mode = LL_GPIO_MODE_INPUT;
+    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
+    LL_GPIO_Init(NFAULT_PORT, &GPIO_InitStruct);
+}
+#endif
+
 void reloadWatchDogCounter()
 {
     LL_IWDG_ReloadCounter(IWDG);
@@ -583,6 +608,18 @@ void enableCorePeripherals()
 
 #ifdef USE_CUSTOM_LED
     initLed();
+#endif
+
+#ifdef USE_DRV8328_NSLEEP
+    initnSleep();
+#endif
+
+#ifdef USE_DRV8328_NFAULT
+    initnFault();
+#endif
+
+#ifdef USE_DRVOFF
+    initDrvoff();
 #endif
 
 #ifndef BRUSHED_MODE

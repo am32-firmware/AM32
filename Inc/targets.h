9a9d239e2d851617f28db6fc81e9222f902ead69
Andrew Tridgell <andrew@tridgell.net>
2024-07-15 13:02:12 +1000

    inc: show an error if missing target defines

--- Inc/targets.h
+++ Inc/targets.h
@@ -1093,6 +1093,11 @@
 #define VOLTAGE_ADC_CHANNEL LL_ADC_CHANNEL_7
 #endif
 
+#ifndef FIRMWARE_NAME
+/* if you get this then you have forgotten to add the section for your target above */
+#error "Missing defines for target"
+#endif
+
 /********************************** defaults if not set
  * ***************************/
 

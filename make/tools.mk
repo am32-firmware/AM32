d249ae9a25daaee2cedf746476bd1ae7f064cd70
Andrew Tridgell <andrew@tridgell.net>
2024-07-23 16:15:54 +1000

    use Mcu specific openocd config files

--- make/tools.mk
+++ make/tools.mk
@@ -12,26 +12,26 @@ LINUX_TOOLS=https://firmware.ardupilot.org/Tools/AM32-tools/linux-tools.tar.gz
 ifeq ($(OS),Windows_NT)
 ARM_SDK_PREFIX:=tools/windows/xpack-arm-none-eabi-gcc-10.3.1-2.3/bin/arm-none-eabi-
 SHELL:=cmd.exe
-COPY:=copy /Y
+COPY:=tools\\windows\\make\\bin\\cp -f
 DSEP:=\\
 NUL:=NUL
-MKDIR:=mkdir
-RMDIR:=del /s /q
+MKDIR:=tools\\windows\\make\\bin\\mkdir -p
+RM:=tools\\windows\\make\\bin\\\rm -rf
 
 arm_sdk_install:
 	@echo "Installing windows tools"
-	@$(RMDIR) tools\\windows
+	@$(RM) -rf tools\\windows
 	@powershell -Command "& { (New-Object System.Net.WebClient).DownloadFile('$(WINDOWS_TOOLS)', 'windows-tools.zip') }"
 	@powershell -Command "Expand-Archive -Path windows-tools.zip -Force -DestinationPath ."
 	@echo "windows tools install done"
 
 else
 ARM_SDK_PREFIX:=tools/linux/xpack-arm-none-eabi-gcc-10.3.1-2.3/bin/arm-none-eabi-
-COPY:=cp
+COPY:=cp -f
 DSEP:=/
 NUL:=/dev/null
 MKDIR:=mkdir -p
-RMDIR:=rm -rf
+RM:=rm -rf
 
 arm_sdk_install:
 	@echo "Installing linux tools"
@@ -40,3 +40,7 @@ arm_sdk_install:
 	@echo "linux tools install done"
 
 endif
+
+# workaround for lack of a lowercase function in GNU make
+# look away before this sends you blind ....
+lc = $(subst A,a,$(subst B,b,$(subst C,c,$(subst D,d,$(subst E,e,$(subst F,f,$(subst G,g,$(subst H,h,$(subst I,i,$(subst J,j,$(subst K,k,$(subst L,l,$(subst M,m,$(subst N,n,$(subst O,o,$(subst P,p,$(subst Q,q,$(subst R,r,$(subst S,s,$(subst T,t,$(subst U,u,$(subst V,v,$(subst W,w,$(subst X,x,$(subst Y,y,$(subst Z,z,$1))))))))))))))))))))))))))

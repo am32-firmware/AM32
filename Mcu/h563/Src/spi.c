e238b70767a4055ac9ea06104edaa4f7ebfe23a9
Jacob Walser <jwalser90@gmail.com>
2024-09-07 06:40:01 -0400

    two words

--- Mcu/h563/Src/spi.c
+++ Mcu/h563/Src/spi.c
@@ -130,29 +130,30 @@ void spi_start_tx_dma_transfer(spi_t* spi)
 
     if (spi->_dma_transfer_count) {
 
-            // disable the spi
-    // spi->ref->CR1 &= ~SPI_CR1_SPE;
-    spi_disable(spi);
+    // disable the spi
+    spi->ref->CR1 &= ~SPI_CR1_SPE;
+
+    // spi->ref->IFCR = 0xffffffff;
     spi->ref->IFCR |= SPI_IFCR_TXTFC;
     // set TSIZE - transfer length in words
     // spi must be disabled to set TSIZE
-    SPI5->CR2 = spi->_dma_transfer_count;
+    spi->ref->CR2 = spi->_dma_transfer_count;
     // SPI5->CR2 = 1;
     
+        if (spi->_dma_transfer_count > 1) {
 
-        spi->txDma->ref->CBR1 = (spi->_dma_transfer_count);
-        spi->txDma->ref->CSAR = (uint32_t)(spi->_tx_buffer + spi->_tx_tail);
-        //spi->ref->ICR |= spi_ICR_TCCF; // maybe not necessary
-        spi->txDma->ref->CCR |= DMA_CCR_EN;
-
-
+            spi->txDma->ref->CBR1 = (spi->_dma_transfer_count);
+            spi->txDma->ref->CSAR = (uint32_t)(spi->_tx_buffer + spi->_tx_tail);
+            //spi->ref->ICR |= spi_ICR_TCCF; // maybe not necessary
+            spi->txDma->ref->CCR |= DMA_CCR_EN;
+        }
+    // enable the spi
+    spi_enable(spi);
 
     // while (spi->txDma->ref->CBR1 == spi->_dma_transfer_count);
 
-    // enable the spi
     // spi->ref->CR1 |= SPI_CR1_SPE;
 
-    spi_enable(spi);
     spi->ref->TXDR = (uint32_t)(spi->_tx_buffer + spi->_tx_tail);
     spi->ref->CR1 |= SPI_CR1_CSTART;
     }

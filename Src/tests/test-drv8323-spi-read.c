92a43118993b62fe7c231b4ce5ab13d58d8e99fd
Jacob Walser <jwalser90@gmail.com>
2024-10-25 00:00:02 -0400

    this does not work

--- Src/tests/test-drv8323-spi-read.c
+++ Src/tests/test-drv8323-spi-read.c
@@ -69,9 +69,9 @@ int main()
     spi.txDma = &dmaChannels[0];
     spi.txDmaRequest = LL_GPDMA1_REQUEST_SPI5_TX;
     spi.rxDmaRequest = LL_GPDMA1_REQUEST_SPI5_RX;
-    // spi.CFG1_MBR = 0b100; // prescaler = 32 // this does not work on blueesc
+    spi.CFG1_MBR = 0b100; // prescaler = 32 // this DOES NOT work on blueesc
     // spi.CFG1_MBR = 0b101; // prescaler = 64 // this works on blueesc
-    spi.CFG1_MBR = 0b100; // prescaler = 128 // this DOES NOT work on blueesc
+    // spi.CFG1_MBR = 0b100; // prescaler = 128 // this DOES NOT work on blueesc
     // spi.CFG1_MBR = 0b111; // prescaler = 256 // this works on blueesc
     spi_initialize(&spi);
 

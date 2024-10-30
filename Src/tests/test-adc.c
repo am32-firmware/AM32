977824b2a6d6a928f10408b4b9ad054af0b04359
Jacob Walser <jwalser90@gmail.com>
2024-10-30 03:19:09 -0400

    test-adc working

--- Src/tests/test-adc.c
+++ Src/tests/test-adc.c
@@ -23,21 +23,21 @@ int main()
     // uint8_t channels[] = {
     // DIE_TEMPERATURE_ADC_CHANNEL};
         uint8_t channels[] = {
-    VOLTAGE_ADC_CHANNEL};
+    DIE_TEMPERATURE_ADC_CHANNEL};
     adc_set_regular_sequence(VOLTAGE_ADC, channels, 1);
     adc_set_sample_time(VOLTAGE_ADC, DIE_TEMPERATURE_ADC_CHANNEL, ADC_SAMPLE_TIME_640_5);
-    // adc_set_continuous_mode(VOLTAGE_ADC, true);
+    adc_set_continuous_mode(VOLTAGE_ADC, true);
     adc_enable(VOLTAGE_ADC);
     adc_start(VOLTAGE_ADC);
     while(1) {
-        while (!(VOLTAGE_ADC->ISR & ADC_ISR_EOS))
-        {
-            // wait
-        }
-        volatile uint32_t d = VOLTAGE_ADC->DR;
+        // while (!(VOLTAGE_ADC->ISR & ADC_ISR_EOS))
+        // {
+        //     // wait
+        // }
+        // volatile uint32_t d = VOLTAGE_ADC->DR;
 
-        // clear end of sequence (EOS) flag
-        VOLTAGE_ADC->ISR |= ADC_ISR_EOS;
-        adc_start(VOLTAGE_ADC);
+        // // clear end of sequence (EOS) flag
+        // VOLTAGE_ADC->ISR |= ADC_ISR_EOS;
+        // adc_start(VOLTAGE_ADC);
     }
 }

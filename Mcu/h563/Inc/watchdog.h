3c92fe8039831e4b008d2e8bc15c87fd4abf88d7
Jacob Walser <jwalser90@gmail.com>
2024-11-02 03:25:42 -0400

    compiles

--- Mcu/h563/Inc/watchdog.h
+++ Mcu/h563/Inc/watchdog.h
@@ -1,7 +1,26 @@
+// independent watchdog (IWDG) driver
+
 #pragma once
 
 #include <inttypes.h>
 
-void watchdog_initialize();
-void watchdog_enable(uint32_t period);
+typedef enum
+{
+    IWDG_PRESCALER_4,
+    IWDG_PRESCALER_8,
+    IWDG_PRESCALER_16,
+    IWDG_PRESCALER_32,
+    IWDG_PRESCALER_64,
+    IWDG_PRESCALER_128,
+    IWDG_PRESCALER_256,
+    IWDG_PRESCALER_512,
+    IWDG_PRESCALER_1024,
+} iwdgPrescaler_e;
+
+void watchdog_set_prescaler();
+void watchdog_initialize(
+    iwdgPrescaler_e prescaler,
+    uint16_t reload);
+void watchdog_enable();
 void watchdog_goodboy_pat();
+void watchdog_set_rlr(uint32_t rlr);
--- Mcu/h563/Src/watchdog.c
+++ Mcu/h563/Src/watchdog.c
@@ -5,9 +5,40 @@
 #include "stm32h5xx_ll_bus.h"
 #include "stm32h5xx_ll_iwdg.h"
 
-void watchdog_initialize()
+
+
+// Bits 15:0 KEY[15:0]: Key value (write only, read 0x0000)
+// These bits can be used for several functions, depending upon the value written by the
+// application:
+// - 0xAAAA: reloads the RL[11:0] value into the IWDCNT down-counter (watchdog refresh),
+// and write-protects registers. This value must be written by software at regular intervals,
+// otherwise the watchdog generates a reset when the counter reaches 0.
+// - 0x5555: enables write-accesses to the registers.
+// - 0xCCCC: enables the watchdog (except if the hardware watchdog option is selected) and
+// write-protects registers.
+// - values different from 0x5555: write-protects registers.
+// Note that only IWDG_PR, IWDG_RLR, IWDG_EWCR and IWDG_WINR registers have a
+// write-protection mechanism.
+void watchdog_enable()
 {
-    
+    // - 0xCCCC: enables the watchdog (except if the hardware watchdog
+    //   option is selected) and write-protects registers.
+    IWDG->KR = 0x0000CCCCU;
+}
+
+void watchdog_unlock()
+{
+    // unlock watchdog via key register
+    // - 0x5555: enables write-accesses
+    //   to the registers.
+    IWDG->KR = 0x00005555U;
+}
+
+void watchdog_initialize(iwdgPrescaler_e prescaler, uint16_t reload)
+{
+
+    watchdog_unlock();
+
 }
 
 void reloadWatchDogCounter()
@@ -19,11 +50,20 @@ void reloadWatchDogCounter()
 void MX_IWDG_Init(void)
 {
     LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_WWDG);
+    // unlock watchdog via key register
     IWDG->KR = 0x0000CCCCU;
     IWDG->KR = 0x00005555U;
+    // set prescaler
     IWDG->PR = LL_IWDG_PRESCALER_16;
+    // set reload register
     IWDG->RLR = 4000;
     // while (IWDG->SR); // wait for the registers to be updated
     IWDG->KR = 0x0000AAAA;
 }
 
+// RLR is a 12 bit value
+// reset value is 0xfff
+void watchdog_set_rlr(uint32_t rlr)
+{
+    IWDG->RLR = rlr;
+}

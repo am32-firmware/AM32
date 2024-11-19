42a17eff30000cadb012bb93a27fd6fa27b3cd9e
Jacob Walser <jwalser90@gmail.com>
2024-11-19 02:08:16 -0500

    test-led-sk6812-spi2 is working with power from aux port

--- Src/tests/test-usart-aux.c
+++ Src/tests/test-usart-aux.c
@@ -1,8 +1,11 @@
-#include "targets.h"
-#include "usart.h"
-#include "gpio.h"
+#include "stm32h563xx.h"
+
 #include "dma.h"
+#include "gpio.h"
 #include "mcu.h"
+#include "usart.h"
+#include "targets.h"
+
 static uint8_t usart_rx_buffer[256];
 static uint8_t usart_tx_buffer[256];
 static usart_t usart;
@@ -10,9 +13,11 @@ static usart_t usart;
 int main()
 {
     mcu_setup();
-
-    dma_initialize();
-    AUX_UART_ENABLE_CLOCK();
+    AUX_USART_ENABLE_CLOCK();
+    gpio_t gpioUsartRx = DEF_GPIO(MAIN_USART_RX_PORT, MAIN_USART_RX_PIN, MAIN_USART_RX_AF, GPIO_AF);
+    gpio_t gpioUsartTx = DEF_GPIO(MAIN_USART_TX_PORT, MAIN_USART_TX_PIN, MAIN_USART_TX_AF, GPIO_AF);
+    gpio_initialize(&gpioUsartRx);
+    gpio_initialize(&gpioUsartTx);
 
     usart.ref = AUX_USART_TX_PERIPH;
 
@@ -22,21 +27,13 @@ int main()
     usart._tx_buffer_size = 256;
     usart.rxDma = &dmaChannels[7];
     usart.txDma = &dmaChannels[0];
-    usart.txDmaRequest = LL_GPDMA1_REQUEST_UART8_TX;
-    usart.rxDmaRequest = LL_GPDMA1_REQUEST_UART8_TX;
+    usart.txDmaRequest = AUX_USART_TX_DMA_REQ;
 
     usart._baudrate = 1000000;
+    usart.swap = 0;
     usart_initialize(&usart);
 
-    gpio_t gpioUsartTx = DEF_GPIO(
-        AUX_USART_TX_PORT,
-        AUX_USART_TX_PIN,
-        AUX_USART_TX_AF,
-        GPIO_AF);
-    gpio_initialize(&gpioUsartTx);
-
     while(1) {
-        // usart_write(&usart, "U", 1);
         usart_write_string(&usart, "hello world\n");
     }
 }

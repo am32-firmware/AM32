77a3a9c747be7e0ffe657d27a6854efb22a9df40
AlkaMotors <37931458+AlkaMotors@users.noreply.github.com>
2024-11-25 11:09:34 -0400

    Do not adjust dead time override if driving brake set to 10

--- Src/main.c
+++ Src/main.c
@@ -667,7 +667,6 @@ void loadEEpromSettings()
             if (dead_time_override > 200) {
                 dead_time_override = 200;
             }
-        }
         min_startup_duty = min_startup_duty + dead_time_override;
         minimum_duty_cycle = minimum_duty_cycle + dead_time_override;
         throttle_max_at_low_rpm = throttle_max_at_low_rpm + dead_time_override;
@@ -681,7 +680,7 @@ void loadEEpromSettings()
 #ifdef GIGADEVICES
         TIMER_CCHP(TIMER0) |= dead_time_override;
 #endif
-
+        }
         if (eepromBuffer.limits.temperature < 70 || eepromBuffer.limits.temperature > 140) {
             eepromBuffer.limits.temperature = 255;
         }

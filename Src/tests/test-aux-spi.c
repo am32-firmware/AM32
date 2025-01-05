934b58213dd82a16da93ce7267409586584c53d6
Jacob Walser <jwalser90@gmail.com>
2025-01-04 22:57:36 -0500

    test-aux-spi works

--- Src/tests/test-aux-spi.c
+++ Src/tests/test-aux-spi.c
@@ -45,20 +45,45 @@ int main()
         AUX_SPI_MOSI_AF,
         GPIO_AF);
 
+    // configure spi kernel clock as HSE via per_ck (25MHz)
+    // spi_configure_rcc_clock_selection(spi, 0b100);
+
     spi->_rx_buffer = spi_rx_buffer;
     spi->_tx_buffer = spi_tx_buffer;
     spi->_rx_buffer_size = 256;
     spi->_tx_buffer_size = 256;
-    spi->rxDma = 0;
+    // spi->rxDma = 0;
+    spi->rxDma = &dmaChannels[AUX_SPI_RX_DMA_CHANNEL];
     spi->txDma = &dmaChannels[AUX_SPI_TX_DMA_CHANNEL];
-    spi->txDmaRequest = AUX_SPI_TX_DMA_REQ;
     spi->rxDmaRequest = AUX_SPI_RX_DMA_REQ;
+    spi->txDmaRequest = AUX_SPI_TX_DMA_REQ;
+
+    // spi->CFG1_MBR = SPI_MBR_DIV_4; // kernel clock / 4
+    // spi->CFG2 = ( SPI_CFG2_SSOE
+    //             | SPI_CFG2_CPHA
+    //             | SPI_CFG2_MASTER
+    //             );
+
+    // a 250MHz kernel clock / 32 gives an SPI clock of 7.8125MHz
+    spi->CFG1_MBR = SPI_MBR_DIV_32; // prescaler = 32
+    spi->CFG2 =
+                ( SPI_CFG2_AFCNTR
+                | SPI_CFG2_SSOE
+                | SPI_CFG2_SSOM
+                | SPI_CFG2_CPHA
+                | SPI_CFG2_MASTER
+                | (0b1111 << SPI_CFG2_MIDI_Pos)
+                | (0b1111 << SPI_CFG2_MSSI_Pos)
+                );
     spi_initialize(spi);
 
     gpio_initialize(&gpioSpiNSS);
     gpio_initialize(&gpioSpiSCK);
     gpio_initialize(&gpioSpiMISO);
     gpio_initialize(&gpioSpiMOSI);
+
+    gpio_configure_pupdr(&gpioSpiMISO, GPIO_PULL_UP);
+    // gpio_configure_pupdr(&gpioSpiMOSI, GPIO_PULL_DOWN);
 
     gpio_set_speed(&gpioSpiNSS, GPIO_SPEED_VERYFAST);
     gpio_set_speed(&gpioSpiSCK, GPIO_SPEED_VERYFAST);

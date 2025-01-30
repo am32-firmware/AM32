2b28cab7448d5d9650940e382cc4c6b6ade34700
Jacob Walser <jwalser90@gmail.com>
2025-01-29 21:47:50 -0500

    test-led-sk6812-spi2 works

--- Src/tests/test-led-sk6812-spi2.c
+++ Src/tests/test-led-sk6812-spi2.c
@@ -10,14 +10,20 @@ int main()
     led_initialize();
 
     while(1) {
+
+        // blue
         led_write(0x00100000);
         for (uint32_t i = 0; i < 0xffffff; i++) {
             asm("nop");
         }
+
+        // red
         led_write(0x00001000);
         for (uint32_t i = 0; i < 0xffffff; i++) {
             asm("nop");
         }
+
+        // green
         led_write(0x00000010);
         for (uint32_t i = 0; i < 0xffffff; i++) {
             asm("nop");

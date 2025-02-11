617351961e78fbc46b7883f1cb65189f1bd169c7
Jacob Walser <jwalser90@gmail.com>
2025-02-11 17:01:31 -0500

    increase advance to /4.0f

--- Src/tests/test-as5048-align-all-commutate-comparator2.c
+++ Src/tests/test-as5048-align-all-commutate-comparator2.c
@@ -227,7 +227,7 @@ int main()
         } else {
             diff = zc_angles[i] - zc_angles[i - 1];
         }
-        zc_angles[i] -= round(diff / 6.0f);
+        zc_angles[i] -= round(diff / 4.0f);
         // zc_angles[i] -= diff;
 
         if (i < 3 || i > num_poles - 3) {
@@ -244,7 +244,7 @@ int main()
     }
 
 
-    bridge_set_run_duty(0x0500);
+    bridge_set_run_duty(0x0700);
 
     // here we are at angle = 0
 

c8aca53adacc0e348411179c45090f4b28139d7d
Jacob Walser <jwalser90@gmail.com>
2025-02-13 23:24:41 -0500

    this runs on samguk but very rough

--- Src/tests/test-sensorless-bldc.c
+++ Src/tests/test-sensorless-bldc.c
@@ -388,7 +388,7 @@ int main()
     // to use the sk6812 led spi interrupt
     comparator_nvic_set_priority(&comp, 1);
 
-    bridge_set_run_duty(0x0200);
+    bridge_set_run_duty(0x0100);
 
     comp_timer_enable();
     blanking_enable();

87d99c6b2ef48fd3ac467a3036b4fb87a3335ff3
Jacob Walser <jwalser90@gmail.com>
2025-02-28 17:55:12 -0500

    test-usart-main works on board 19

--- Makefile
+++ Makefile
@@ -118,7 +118,7 @@ $(info    $(SRC_COMMON))
 # SRC_MAIN := $(SRC_DIRS_COMMON)/tests/test-led-sk6812-spi7.c
 # SRC_MAIN := $(SRC_DIRS_COMMON)/tests/test-led.c
 # SRC_MAIN := $(SRC_DIRS_COMMON)/tests/test-led-maxfrequency.c
-SRC_MAIN := $(SRC_DIRS_COMMON)/tests/test-main-gpio.c
+# SRC_MAIN := $(SRC_DIRS_COMMON)/tests/test-main-gpio.c
 # SRC_MAIN := $(SRC_DIRS_COMMON)/tests/test-nothing.c
 # SRC_MAIN := $(SRC_DIRS_COMMON)/tests/test-rng.c
 # SRC_MAIN := $(SRC_DIRS_COMMON)/tests/test-phaseouts.c
@@ -132,7 +132,7 @@ SRC_MAIN := $(SRC_DIRS_COMMON)/tests/test-main-gpio.c
 # SRC_MAIN := $(SRC_DIRS_COMMON)/tests/test-ten-khz-timer.c
 # SRC_MAIN := $(SRC_DIRS_COMMON)/tests/test-usart-aux.c
 # SRC_MAIN := $(SRC_DIRS_COMMON)/tests/test-usart-debug.c
-# SRC_MAIN := $(SRC_DIRS_COMMON)/tests/test-usart-main.c
+SRC_MAIN := $(SRC_DIRS_COMMON)/tests/test-usart-main.c
 # SRC_MAIN := $(SRC_DIRS_COMMON)/tests/test-usart-main-rs485.c
 # SRC_MAIN := $(SRC_DIRS_COMMON)/tests/test-vreg-5V.c
 # SRC_MAIN := $(SRC_DIRS_COMMON)/tests/test-watchdog-fail.c
--- Src/tests/test-usart-main.c
+++ Src/tests/test-usart-main.c
@@ -5,6 +5,7 @@
 #include "mcu.h"
 #include "usart.h"
 #include "targets.h"
+#include "vreg.h"
 
 static uint8_t usart_rx_buffer[256];
 static uint8_t usart_tx_buffer[256];
@@ -13,6 +14,19 @@ static usart_t usart;
 int main()
 {
     mcu_setup(250);
+
+    vreg5V_initialize();
+    vreg5V_enable();
+
+    gpio_t gpioRS485Enable = DEF_GPIO(
+        RS485_ENABLE_PORT,
+        RS485_ENABLE_PIN,
+        0,
+        GPIO_OUTPUT);
+    gpio_initialize(&gpioRS485Enable);
+    gpio_set_speed(&gpioRS485Enable, GPIO_SPEED_VERYFAST);
+    gpio_reset(&gpioRS485Enable);
+
     MAIN_USART_ENABLE_CLOCK();
     gpio_t gpioUsartRx = DEF_GPIO(MAIN_USART_RX_PORT, MAIN_USART_RX_PIN, MAIN_USART_RX_AF, GPIO_AF);
     gpio_t gpioUsartTx = DEF_GPIO(MAIN_USART_TX_PORT, MAIN_USART_TX_PIN, MAIN_USART_TX_AF, GPIO_AF);
@@ -21,6 +35,7 @@ int main()
     gpio_set_speed(&gpioUsartRx, GPIO_SPEED_VERYFAST);
     gpio_set_speed(&gpioUsartTx, GPIO_SPEED_VERYFAST);
 
+    gpio_configure_pupdr(&gpioUsartRx, GPIO_PULL_UP);
     gpio_configure_pupdr(&gpioUsartTx, GPIO_PULL_UP);
 
     usart.ref = MAIN_USART_REF;
@@ -31,9 +46,10 @@ int main()
     usart._tx_buffer_size = 256;
     usart.rxDma = &dmaChannels[7];
     usart.txDma = &dmaChannels[0];
-    usart.txDmaRequest = MAIN_USART_DMA_REQ;
+    usart.rxDmaRequest = MAIN_USART_RX_DMA_REQ;
+    usart.txDmaRequest = MAIN_USART_TX_DMA_REQ;
 
-    usart._baudrate = 3000000;
+    usart._baudrate = 12000000;
     usart.swap = 0;
     usart_initialize(&usart);
 

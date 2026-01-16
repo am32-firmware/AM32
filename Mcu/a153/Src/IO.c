am32-firmware/AM32

commit 2732edc871d6aada604f3583621bb0b6af86aeb6

Fixed bug in sending Dshot telemetry
- Fixed bug that Dshot telemetry was sent after two Dshot receives.
- Dshot timer that is also used for timeout now does not reset the timer when a timeout happens. It checks in the interrupt if the timeout is valid, in order to prevent a timer reset during the first Dshot bit.
- Added check if current input is Dshot in the inverted Dshot check.
- Added transfercomplete() and input_ready = 1 in the SPI interrupt handler that is called when Dshot telemetry has been completed.
- Cleaned up some code a bit and added better comments.

Note: there is a lot of commented debug code in this commit, so it needs some cleaning!

Mcu/a153/Src/IO.c
@@ -16,6 +16,7 @@ extern volatile uint32_t gcrnumber;
 
 void receiveDshotDma()
 {
+	//Set output variable for state machine
 	out_put = 0;
 
 	//Set prescaler
@@ -24,16 +25,18 @@ void receiveDshotDma()
 	if (buffersize > 3) {
 		//Resets PWM/Dshot timer to 0. Needed for Dshot to work properly.
 		resetInputCaptureTimer();
+//		CTIMER0->TC = 0;
 
 		//Set match1 value to higher then the minimum Dshot300 frame time which is around 53us, so take at least 53us.
 		CTIMER0->MR[1] = 10000 / (CTIMER0->PR + 1);
 
 		//Reset timer and enable interrupt on Match1 event
-		modifyReg32(&CTIMER0->MCR, 0, CTIMER_MCR_MR1I(1) | CTIMER_MCR_MR1R(1));
+//		modifyReg32(&CTIMER0->MCR, 0, CTIMER_MCR_MR1I(1) | CTIMER_MCR_MR1R(1));
+		modifyReg32(&CTIMER0->MCR, CTIMER_MCR_MR1I_MASK | CTIMER_MCR_MR1R_MASK, CTIMER_MCR_MR1I(1));
 
 	} else {
 		//Disable interrupt and reset on Match1 event
-		modifyReg32(&CTIMER0->MCR, CTIMER_MCR_MR1I(1) | CTIMER_MCR_MR1R(1), 0);
+		modifyReg32(&CTIMER0->MCR, CTIMER_MCR_MR1I_MASK | CTIMER_MCR_MR1R_MASK, 0);
 	}
 
 	//Set the major loop count and addresses again to prevent unintended DMA request from CTIMER match register
@@ -63,54 +66,42 @@ void receiveDshotDma()
 
 void sendDshotDma()
 {
-//	gcrnumber = 0x1a55aa;
+	//Set output variable for state machine
+	out_put = 1;
 
-//	while (1){
-//		RELOAD_WATCHDOG_COUNTER();
+	//Reset Dshot timer to prevent a timeout from happening during Dshot sending
+//	resetInputCaptureTimer();
 
-		GPIO3->PTOR = (1 << 27);	//ENC_A
+	//Disable interrupt and reset on Match1 event
+//	modifyReg32(&CTIMER0->MCR, CTIMER_MCR_MR1I_MASK | CTIMER_MCR_MR1R_MASK, 0);
 
-		//Change Dshot pin to SPI0_SDI
-		modifyReg32(&INPUT_PIN_PORT->PCR[INPUT_PIN],
-				PORT_PCR_MUX_MASK | PORT_PCR_IBE_MASK | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK,
-				PORT_PCR_MUX(2) | PORT_PCR_IBE(0) | PORT_PCR_PE(1) | PORT_PCR_PS(1));
+	//Change Dshot pin to SPI0_SDI
+	modifyReg32(&INPUT_PIN_PORT->PCR[INPUT_PIN],
+			PORT_PCR_MUX_MASK | PORT_PCR_IBE_MASK | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK,
+			PORT_PCR_MUX(2) | PORT_PCR_IBE(0) | PORT_PCR_PE(1) | PORT_PCR_PS(1));
 
-		//Functional LPSPI0 clock is 48MHz. To get 5/4 x Dshot bit rate, SPI prescaler should be:
-		//Dshot600 => prescaler = 64
-		//Dshot300 => prescaler = 128
+	//Functional LPSPI0 clock is 12MHz. To get 5/4 x Dshot bit rate, SPI prescaler should be:
+	//Dshot600 => prescaler = 8
+	//Dshot300 => prescaler = 16
 
-		//Check if Dshot600
-		if (ic_timer_prescaler) {
-			//Set transmit prescaler to 64 for Dshot600
-			//Set framesize to 21 bits
-			modifyReg32(&LPSPI0->TCR,
-					LPSPI_TCR_PRESCALE_MASK | LPSPI_TCR_FRAMESZ_MASK,
-					LPSPI_TCR_PRESCALE(3) | LPSPI_TCR_FRAMESZ(20) | LPSPI_TCR_RXMSK(1));
-		//Is Dshot300
-		} else {
-			//Set transmit prescaler to 128 for Dshot300
-			//Set framesize to 21 bits
-			modifyReg32(&LPSPI0->TCR,
-					LPSPI_TCR_PRESCALE_MASK | LPSPI_TCR_FRAMESZ_MASK,
-					LPSPI_TCR_PRESCALE(4) | LPSPI_TCR_FRAMESZ(20) | LPSPI_TCR_RXMSK(1));
-		}
+	//Check if Dshot600
+	if (ic_timer_prescaler) {
+		//Set transmit prescaler to 8 for Dshot600
+		//Set framesize to 21 bits
+		modifyReg32(&LPSPI0->TCR,
+				LPSPI_TCR_PRESCALE_MASK | LPSPI_TCR_FRAMESZ_MASK,
+				LPSPI_TCR_PRESCALE(3) | LPSPI_TCR_FRAMESZ(20) | LPSPI_TCR_RXMSK(1));
+	//Is Dshot300
+	} else {
+		//Set transmit prescaler to 16 for Dshot300
+		//Set framesize to 21 bits
+		modifyReg32(&LPSPI0->TCR,
+				LPSPI_TCR_PRESCALE_MASK | LPSPI_TCR_FRAMESZ_MASK,
+				LPSPI_TCR_PRESCALE(4) | LPSPI_TCR_FRAMESZ(20) | LPSPI_TCR_RXMSK(1));
+	}
 
-//		while (!(LPSPI0->SR & LPSPI_SR_TDF_MASK)) {
-//		    // Wait until transmit FIFO is ready
-//		}
-
-		//Send GCR data to LPSPI FIFO
-		LPSPI0->TDR = gcrnumber;
-
-//		while (!(LPSPI0->SR & LPSPI_SR_TCF_MASK)) {
-//		    // Wait until transfer complete
-//		}
-//
-//		LPSPI0->SR = LPSPI_SR_TCF_MASK | LPSPI_SR_FCF_MASK | LPSPI_SR_WCF_MASK;
-
-		//Set output variable for state machine
-		out_put = 1;
-//	}
+	//Send GCR data to LPSPI FIFO
+	LPSPI0->TDR = gcrnumber;
 }
 
 uint8_t getInputPinState()

Mcu/a153/Src/mcxa153_it.c
@@ -34,6 +34,8 @@ extern uint16_t zero_input_count;
 extern uint32_t average_packet_length;
 extern uint8_t average_count;
 
+extern char dshot;
+
 /*
  * @brief 	Comparator 0 interrupt handler. Is called after a compare event has occurred.
  */
@@ -69,11 +71,44 @@ void CMP1_IRQHandler(void)
 }
 
 /*
- * @brief DSHOT CTIMER0 interrupt called when Dshot frame has ended
+ * @brief 	DSHOT CTIMER0 interrupt called after a timeout in which no Dshot frame has been detected.
+ * 			This is to reset the DMA before a new Dshot frame happens.
  */
 void CTIMER0_IRQHandler(void)
 {
 	uint32_t flags = CTIMER0->IR;
+
+	//TODO remove this
+	GPIO3->PTOR = (1 << 27);	//ENC_A
+
+	//If second rising/falling edge of the dshot frame is expected. Do not clear the timer.
+	//Second rising/falling edge will be written to the dma_buffer address + 4 bytes offset.
+//	if (DMA0->CH[DMA_CH_DshotPWM].TCD_DADDR != (uint32_t)&dma_buffer + 4) {
+//		CTIMER0->TC = 0;
+
+//		//Set latest capture 1 value to counter value to prevent missing the first falling/rising edge of a Dshot frame
+//		//This is because the DMA triggers after seeing a rising and falling or a falling and rising after each other.
+//		//This means that the timeout could happen in between the first bit of a Dshot frame,
+//		//causing the first rising/falling time to be missing.
+//		modifyReg32(&CTIMER0->TC, CTIMER_TC_TCVAL_MASK, CTIMER_TC_TCVAL(CTIMER0->CR[1]));
+//	}
+
+	//Check if CR[1] > 0 and CR[2] > 0
+	//If only CR[1] > 0 then the timeout is happening in the first Dshot bit
+
+	//Check if capture 1 is larger than capture 2.
+	//If this is the case, it must be that this is an invalid timeout in the first Dshot bit
+	//So if this is not the case, clear the timer interrupt as the timeout is valid
+	//Also check if the destination address is the first element of the dma_buffer
+	if (!((CTIMER0->CR[1] > CTIMER0->CR[2]) && (DMA0->CH[DMA_CH_DshotPWM].TCD_DADDR == (uint32_t)&dma_buffer))) {
+//		resetInputCaptureTimer();
+		CTIMER0->TC = 0;
+	}
+	else
+	{
+		//TODO remove this
+		GPIO3->PTOR = (1 << 28);	//ENC_I
+	}
 
 	//Set the major loop count and addresses again to prevent unintended DMA request from CTIMER match register
 	//Set current and beginning major loop count to 8
@@ -93,11 +128,14 @@ void CTIMER0_IRQHandler(void)
 	//Set destination address
 	DMA0->CH[DMA_CH_DshotPWM].TCD_DADDR = (uint32_t)&dma_buffer;
 
-	//Set latest capture 1 value to counter value to prevent it being skipped
-	modifyReg32(&CTIMER0->TC, CTIMER_TC_TCVAL_MASK, CTIMER_TC_TCVAL(CTIMER0->CR[1]));
+	//Set latest capture 1 value to counter value to prevent missing the first falling/rising edge of a Dshot frame
+	//This is because the DMA triggers after seeing a rising and falling or a falling and rising after each other.
+	//This means that the timeout could happen in between the first bit of a Dshot frame,
+	//causing the first rising/falling time to be missing.
+//	modifyReg32(&CTIMER0->TC, CTIMER_TC_TCVAL_MASK, CTIMER_TC_TCVAL(CTIMER0->CR[1]));
 
 	//Check for inverted Dshot
-	if (!armed) {
+	if (!armed && dshot) {
 		if (is_inverted_dshot == 0) {
 			//Check if signal pin is high
 			if (getInputPinState()) {
@@ -183,8 +221,20 @@ void DMA_CH0_IRQHandler(void)
 	//Convert to correct Dshot/PWM timing data format
 	doDshotCorrection();
 
-	//Call transfercomplete
-	transfercomplete();
+//    if (armed && dshot_telemetry) {
+//        if (out_put) {
+//            receiveDshotDma();
+//            compute_dshot_flag = 2;
+//        } else {
+//            sendDshotDma();
+//            compute_dshot_flag = 1;
+//        }
+//    }
+//    else
+//    {
+		//Call transfercomplete
+		transfercomplete();
+//    }
 
 	//Set input_ready so processDshot is called in main loop
 	input_ready = 1;
@@ -233,8 +283,31 @@ void LPSPI0_IRQHandler(void)
 
 	//Set PWM/Dshot input pin to timer capture/compare input
 	//Enable input buffer and disable pull-up/down resistor
-	modifyReg32(&INPUT_PIN_PORT->PCR[INPUT_PIN],
-			PORT_PCR_MUX_MASK | PORT_PCR_IBE_MASK | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK,
-			PORT_PCR_MUX(INPUT_PIN_ALT_FUNC) | PORT_PCR_IBE(1) | PORT_PCR_PE(0) | PORT_PCR_PS(1));
+//	modifyReg32(&INPUT_PIN_PORT->PCR[INPUT_PIN],
+//			PORT_PCR_MUX_MASK | PORT_PCR_IBE_MASK | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK,
+//			PORT_PCR_MUX(INPUT_PIN_ALT_FUNC) | PORT_PCR_IBE(1) | PORT_PCR_PE(0) | PORT_PCR_PS(1));
+
+//	out_put = 0;
+
+	//Reset Dshot timer to prevent a timeout from happening right after transfer
+//	resetInputCaptureTimer();
+
+	//Resets PWM/Dshot timer to 0. Needed for Dshot to work properly.
+//	resetInputCaptureTimer();
+//	CTIMER0->TC = 0;
+
+	//Set match1 value to higher then the minimum Dshot300 frame time which is around 53us, so take at least 53us.
+//	CTIMER0->MR[1] = 10000 / (CTIMER0->PR + 1);
+
+	//Reset timer and enable interrupt on Match1 event
+//	modifyReg32(&CTIMER0->MCR, CTIMER_MCR_MR1I_MASK | CTIMER_MCR_MR1R_MASK, CTIMER_MCR_MR1I(1));
+
+	transfercomplete();
+
+	input_ready = 1;
+
+//	receiveDshotDma();
+
+//	DMA_CH0_IRQHandler();
 }
 

Src/main.c
@@ -406,7 +406,7 @@ void loadEEpromSettings()
     eepromBuffer.limits.temperature = 141;
     eepromBuffer.limits.current = 102;
     eepromBuffer.sine_mode_power = 6;
-    eepromBuffer.telemetry_on_interval = 1;
+    eepromBuffer.telemetry_on_interval = 0;
     eepromBuffer.input_type = 0;	//Sets Dshot input
 
     eepromBuffer.tune[0] = 0xff;	//Turn off BJ tune
@@ -1871,8 +1871,9 @@ if(zero_crosses < 5){
 #endif
         if (send_telemetry) {
 #ifdef USE_SERIAL_TELEMETRY
-            makeTelemPackage((int8_t)degrees_celsius, battery_voltage, actual_current,
-                (uint16_t)(consumed_current >> 16), e_rpm);
+//            makeTelemPackage((int8_t)degrees_celsius, battery_voltage, actual_current,
+//                (uint16_t)(consumed_current >> 16), e_rpm);
+        	makeTelemPackage(0x0d, battery_voltage, 0, 0, 0);
             send_telem_DMA(10);
             send_telemetry = 0;
 #endif

.settings/language.settings.xml
@@ -5,7 +5,7 @@
 			<provider copy-of="extension" id="org.eclipse.cdt.ui.UserLanguageSettingsProvider"/>
 			<provider-reference id="org.eclipse.cdt.core.ReferencedProjectsLanguageSettingsProvider" ref="shared-provider"/>
 			<provider copy-of="extension" id="org.eclipse.cdt.managedbuilder.core.GCCBuildCommandParser"/>
-			<provider class="org.eclipse.cdt.managedbuilder.language.settings.providers.GCCBuiltinSpecsDetector" console="false" env-hash="1606774695304" id="org.eclipse.cdt.managedbuilder.core.GCCBuiltinSpecsDetector" keep-relative-paths="false" name="CDT GCC Built-in Compiler Settings" parameter="${COMMAND} ${FLAGS} -E -P -v -dD &quot;${INPUTS}&quot;" prefer-non-shared="true">
+			<provider class="org.eclipse.cdt.managedbuilder.language.settings.providers.GCCBuiltinSpecsDetector" console="false" env-hash="-1680604880057" id="org.eclipse.cdt.managedbuilder.core.GCCBuiltinSpecsDetector" keep-relative-paths="false" name="CDT GCC Built-in Compiler Settings" parameter="${COMMAND} ${FLAGS} -E -P -v -dD &quot;${INPUTS}&quot;" prefer-non-shared="true">
 				<language-scope id="org.eclipse.cdt.core.gcc"/>
 				<language-scope id="org.eclipse.cdt.core.g++"/>
 			</provider>

Mcu/a153/Src/DMA.c
@@ -297,7 +297,20 @@ void doDshotCorrection(void)
 {
 	//If larger than 3 it must be Dshot. PWM input does not need correction
 	if (buffersize > 3) {
+		//TODO remove this
+//		volatile uint32_t dshot_time_diffs[32];
+//		volatile uint32_t decoded_dshot_data[32];
+//		for (int i = 0; i < 32; i++) {
+//			dshot_time_diffs[i] = dma_buffer[(i << 1) + 1] - dma_buffer[(i << 1)];
+//			decoded_dshot_data[i] = dshot_time_diffs[i] > 38;
+//		}
+
 		int sum = dma_buffer[1];
+
+		//TODO remove this
+//		sum = decoded_dshot_data[1];
+//		sum = dma_buffer[1];
+
 		int temp_sum = sum;
 
 		for (int i = 1; i < (buffersize / 2); i++) {

Mcu/a153/Src/timers.c
@@ -49,6 +49,7 @@ void MX_IWDG_Init(void)
  * 			On the falling edge the timer is cleared, which causes a match0 event that triggers a DMA0 request.
  * 			The DMA then transfers the capture 1 and capture 2 counter values to the DMA buffer.
  * 			To reconstruct the Dshot/PWM data a correction is done after the DMA buffer is fully filled.
+ * 			CTIMER0 is configured to run at functional clock of 96MHz
  */
 void initDshotPWMTimer(void)
 {
@@ -89,11 +90,16 @@ void initDshotPWMTimer(void)
 	CTIMER0->MR[0] = 0;
 
 	//Set match1 value to higher then the minimum Dshot300 frame time which is around 53us, so take at least 53us.
-	//Set to 6000 => approximately 60us.
+	//Functional clock is 96MHz. So Dshot300 frame is around 5000 clock ticks.
+	//Set match event to trigger at twice the frame time.
+	//And correct for prescaler.
 	CTIMER0->MR[1] = 10000 / (CTIMER0->PR + 1);
 
 	//Reset timer and enable interrupt on Match1 event
-	modifyReg32(&CTIMER0->MCR, 0, CTIMER_MCR_MR1I(1) | CTIMER_MCR_MR1R(1));
+//	modifyReg32(&CTIMER0->MCR, 0, CTIMER_MCR_MR1I(1) | CTIMER_MCR_MR1R(1));
+
+	//Enable interrupt on Match1 event
+	modifyReg32(&CTIMER0->MCR, 0, CTIMER_MCR_MR1I(1));
 
 	//Configure capture control register so capture value register is loaded on CR1 rising edge and CR2 falling edge
 	modifyReg32(&CTIMER0->CCR,

Src/dshot.c
@@ -48,6 +48,12 @@ uint8_t programming_mode;
 uint16_t position;
 uint8_t  new_byte;
 
+//TODO remove this
+uint32_t avg_data_arr[100];
+uint32_t avg_window = 100;
+uint32_t avg_data = 0;
+uint8_t avg_idx = 0;
+
 void computeDshotDMA()
 {
     dshot_frametime = dma_buffer[31] - dma_buffer[0];
@@ -80,11 +86,33 @@ void computeDshotDMA()
 
         int tocheck = (dpulse[0] << 10 | dpulse[1] << 9 | dpulse[2] << 8 | dpulse[3] << 7 | dpulse[4] << 6 | dpulse[5] << 5 | dpulse[6] << 4 | dpulse[7] << 3 | dpulse[8] << 2 | dpulse[9] << 1 | dpulse[10]);
 
+        //TODO remove this
+        //average tocheck data and send telemetry if different
+//        avg_data_arr[avg_idx++] = tocheck;
+//
+//        //Correct index roll-over
+//        if (avg_idx >= avg_window) {
+//			avg_idx = 0;
+//		}
+//
+//        //Average data
+//        avg_data = 0;
+//        for (uint32_t i = 0; i < avg_window; ++i) {
+//			avg_data += avg_data_arr[i];
+//		}
+//        avg_data = avg_data / avg_window;
+//
+//        //Check if new value is out of bound
+//        if (avg_data != (uint32_t)tocheck) {
+//        	send_telemetry = 1;
+//        	battery_voltage = (uint16_t)tocheck;
+//		}
+        //TODO remove untill here
+
         if (calcCRC == checkCRC) {
             signaltimeout = 0;
             dshot_goodcounts++;
             if (dpulse[11] == 1) {
-//            if (dpulse[10] == 1) {	//TODO remove this
                 send_telemetry = 1;
             }
             if(programming_mode > 0){  
@@ -273,6 +301,8 @@ void make_dshot_package(uint16_t com_time)
     }
     gcr[buffer_padding] = 0;
 #elif defined(NXP)
+    //Do grey encoding
+    //Apparently GCR RLL is not done here but Grey encoding.
     uint32_t binary = gcrnumber;
     while (gcrnumber >>= 1) {
         binary ^= gcrnumber;

commit 3e34c9c7e6d28c27d8781c9a988d9a3acc23e189

MCXA153 Fixed bug where dshot was not handled correctly and introduced motor stutter
After commit 2732edc871d6aada604f3583621bb0b6af86aeb6, which fixed a bug in sending dshot telemetry introduced the motor stutter.
Now the Dshot telemetry code is reverted so it is similar to commit d77feddabd051d5e1177ee499edf109356813050, where Dshot telemetry is implemented for the first time.

Mcu/a153/Src/IO.c
@@ -31,8 +31,8 @@ void receiveDshotDma()
 		CTIMER0->MR[1] = 10000 / (CTIMER0->PR + 1);
 
 		//Reset timer and enable interrupt on Match1 event
-//		modifyReg32(&CTIMER0->MCR, 0, CTIMER_MCR_MR1I(1) | CTIMER_MCR_MR1R(1));
-		modifyReg32(&CTIMER0->MCR, CTIMER_MCR_MR1I_MASK | CTIMER_MCR_MR1R_MASK, CTIMER_MCR_MR1I(1));
+		modifyReg32(&CTIMER0->MCR, 0, CTIMER_MCR_MR1I(1) | CTIMER_MCR_MR1R(1));
+//		modifyReg32(&CTIMER0->MCR, CTIMER_MCR_MR1I_MASK | CTIMER_MCR_MR1R_MASK, CTIMER_MCR_MR1I(1));
 
 	} else {
 		//Disable interrupt and reset on Match1 event

Mcu/a153/Src/mcxa153_it.c
@@ -100,15 +100,15 @@ void CTIMER0_IRQHandler(void)
 	//If this is the case, it must be that this is an invalid timeout in the first Dshot bit
 	//So if this is not the case, clear the timer interrupt as the timeout is valid
 	//Also check if the destination address is the first element of the dma_buffer
-	if (!((CTIMER0->CR[1] > CTIMER0->CR[2]) && (DMA0->CH[DMA_CH_DshotPWM].TCD_DADDR == (uint32_t)&dma_buffer))) {
-//		resetInputCaptureTimer();
-		CTIMER0->TC = 0;
-	}
-	else
-	{
-		//TODO remove this
-		GPIO3->PTOR = (1 << 28);	//ENC_I
-	}
+//	if (!((CTIMER0->CR[1] > CTIMER0->CR[2]) && (DMA0->CH[DMA_CH_DshotPWM].TCD_DADDR == (uint32_t)&dma_buffer))) {
+////		resetInputCaptureTimer();
+//		CTIMER0->TC = 0;
+//	}
+//	else
+//	{
+//		//TODO remove this
+//		GPIO3->PTOR = (1 << 28);	//ENC_I
+//	}
 
 	//Set the major loop count and addresses again to prevent unintended DMA request from CTIMER match register
 	//Set current and beginning major loop count to 8
@@ -132,10 +132,11 @@ void CTIMER0_IRQHandler(void)
 	//This is because the DMA triggers after seeing a rising and falling or a falling and rising after each other.
 	//This means that the timeout could happen in between the first bit of a Dshot frame,
 	//causing the first rising/falling time to be missing.
-//	modifyReg32(&CTIMER0->TC, CTIMER_TC_TCVAL_MASK, CTIMER_TC_TCVAL(CTIMER0->CR[1]));
+	modifyReg32(&CTIMER0->TC, CTIMER_TC_TCVAL_MASK, CTIMER_TC_TCVAL(CTIMER0->CR[1]));
 
 	//Check for inverted Dshot
-	if (!armed && dshot) {
+//	if (!armed && dshot) {
+	if (!armed) {
 		if (is_inverted_dshot == 0) {
 			//Check if signal pin is high
 			if (getInputPinState()) {
@@ -283,9 +284,9 @@ void LPSPI0_IRQHandler(void)
 
 	//Set PWM/Dshot input pin to timer capture/compare input
 	//Enable input buffer and disable pull-up/down resistor
-//	modifyReg32(&INPUT_PIN_PORT->PCR[INPUT_PIN],
-//			PORT_PCR_MUX_MASK | PORT_PCR_IBE_MASK | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK,
-//			PORT_PCR_MUX(INPUT_PIN_ALT_FUNC) | PORT_PCR_IBE(1) | PORT_PCR_PE(0) | PORT_PCR_PS(1));
+	modifyReg32(&INPUT_PIN_PORT->PCR[INPUT_PIN],
+			PORT_PCR_MUX_MASK | PORT_PCR_IBE_MASK | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK,
+			PORT_PCR_MUX(INPUT_PIN_ALT_FUNC) | PORT_PCR_IBE(1) | PORT_PCR_PE(0) | PORT_PCR_PS(1));
 
 //	out_put = 0;
 
@@ -302,9 +303,9 @@ void LPSPI0_IRQHandler(void)
 	//Reset timer and enable interrupt on Match1 event
 //	modifyReg32(&CTIMER0->MCR, CTIMER_MCR_MR1I_MASK | CTIMER_MCR_MR1R_MASK, CTIMER_MCR_MR1I(1));
 
-	transfercomplete();
-
-	input_ready = 1;
+//	transfercomplete();
+//
+//	input_ready = 1;
 
 //	receiveDshotDma();
 

Src/main.c
@@ -1894,9 +1894,8 @@ if(zero_crosses < 5){
 #endif
         if (send_telemetry) {
 #ifdef USE_SERIAL_TELEMETRY
-//            makeTelemPackage((int8_t)degrees_celsius, battery_voltage, actual_current,
-//                (uint16_t)(consumed_current >> 16), e_rpm);
-        	makeTelemPackage(0x0d, battery_voltage, 0, 0, 0);
+            makeTelemPackage((int8_t)degrees_celsius, battery_voltage, actual_current,
+                (uint16_t)(consumed_current >> 16), e_rpm);
             send_telem_DMA(10);
             send_telemetry = 0;
 #endif
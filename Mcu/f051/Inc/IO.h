a7fb689b7d46af5f20165e1260b66fedafd5795b
Alkamotors <alkazorith@gmail.com>
2026-02-23 11:14:29 -0400

    Change telemetry to report negative temperature

--- Mcu/f051/Inc/IO.h
+++ Mcu/f051/Inc/IO.h
@@ -25,7 +25,6 @@ void enableHalfTransferInt();
 void setInputPullNone();
 
 extern char send_telemetry;
-extern uint8_t degrees_celsius;
 
 extern uint16_t ADC_raw_volts;
 extern uint16_t servo_low_threshold; // anything below this point considered 0

aca26fd70a1781be42a4deeed9579b3b746731e7
dtrbinh <dotranbinhqng02@gmail.com>
2026-06-08 11:32:40 +0700

    f421: honor USE_PA14_TELEMETRY for serial telemetry

--- Mcu/f421/Src/serial_telemetry.c
+++ Mcu/f421/Src/serial_telemetry.c
@@ -7,7 +7,61 @@
 
 #include "serial_telemetry.h"
 #include "common.h"
+#include "targets.h"
 #include "kiss_telemetry.h"
+
+#ifdef USE_PA14_TELEMETRY
+
+void send_telem_DMA(uint8_t bytes)
+{ // set data length and enable channel to start transfer
+    DMA1_CHANNEL4->ctrl_bit.chen = FALSE;
+    DMA1_CHANNEL4->dtcnt = bytes;
+    DMA1_CHANNEL4->ctrl_bit.chen = TRUE;
+}
+
+void telem_UART_Init(void)
+{
+    gpio_init_type gpio_init_struct;
+
+    crm_periph_clock_enable(CRM_USART2_PERIPH_CLOCK, TRUE);
+    crm_periph_clock_enable(CRM_GPIOA_PERIPH_CLOCK, TRUE);
+    crm_periph_clock_enable(CRM_DMA1_PERIPH_CLOCK, TRUE);
+
+    /* configure the usart2 tx pin on PA14 (SWCLK/TLM) */
+    gpio_init_struct.gpio_drive_strength = GPIO_DRIVE_STRENGTH_STRONGER;
+    gpio_init_struct.gpio_out_type = GPIO_OUTPUT_PUSH_PULL;
+    gpio_init_struct.gpio_mode = GPIO_MODE_MUX;
+    gpio_init_struct.gpio_pins = GPIO_PINS_14;
+    gpio_init_struct.gpio_pull = GPIO_PULL_UP;
+    gpio_init(GPIOA, &gpio_init_struct);
+    gpio_pin_mux_config(GPIOA, GPIO_PINS_SOURCE14, GPIO_MUX_1);
+
+    dma_reset(DMA1_CHANNEL4);
+
+    dma_init_type dma_init_struct;
+    dma_default_para_init(&dma_init_struct);
+    dma_init_struct.buffer_size = sizeof(aTxBuffer);
+    dma_init_struct.direction = DMA_DIR_MEMORY_TO_PERIPHERAL;
+    dma_init_struct.memory_base_addr = (uint32_t)aTxBuffer;
+    dma_init_struct.memory_data_width = DMA_MEMORY_DATA_WIDTH_BYTE;
+    dma_init_struct.memory_inc_enable = TRUE;
+    dma_init_struct.peripheral_base_addr = (uint32_t)&USART2->dt;
+    dma_init_struct.peripheral_data_width = DMA_PERIPHERAL_DATA_WIDTH_BYTE;
+    dma_init_struct.peripheral_inc_enable = FALSE;
+    dma_init_struct.priority = DMA_PRIORITY_LOW;
+    dma_init_struct.loop_mode_enable = FALSE;
+    dma_init(DMA1_CHANNEL4, &dma_init_struct);
+
+    /* configure usart2 param */
+    usart_init(USART2, 115200, USART_DATA_8BITS, USART_STOP_1_BIT);
+    usart_transmitter_enable(USART2, TRUE);
+    usart_receiver_enable(USART2, TRUE);
+    usart_single_line_halfduplex_select(USART2, TRUE);
+    usart_dma_transmitter_enable(USART2, TRUE);
+    usart_enable(USART2, TRUE);
+}
+
+#else
 
 void send_telem_DMA(uint8_t bytes)
 { // set data length and enable channel to start transfer
@@ -62,3 +116,5 @@ void telem_UART_Init(void)
 
     nvic_irq_enable(DMA1_Channel3_2_IRQn, 3, 0);
 }
+
+#endif
